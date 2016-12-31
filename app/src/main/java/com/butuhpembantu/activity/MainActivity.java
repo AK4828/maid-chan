66a17e5f9980bcb0691d06ee05502f15caef0354
ari_akm <aremaakm@gmail.com>
2016-12-31 20:38:06 +0700

    drawer layout

--- app/src/main/java/com/butuhpembantu/activity/MainActivity.java
+++ app/src/main/java/com/butuhpembantu/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.butuhpembantu.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.butuhpembantu.R;
 import com.daimajia.slider.library.Indicators.PagerIndicator;
@@ -24,8 +28,14 @@ public class MainActivity extends AppCompatActivity {
     SliderLayout promoSlider;
     @BindView(R.id.slider_indicator)
     PagerIndicator sliderIndicator;
-    @BindView(R.id.activity_main)
-    LinearLayout activityMain;
+    @BindView(R.id.service_name)
+    TextView serviceName;
+    @BindView(R.id.service_name1)
+    TextView serviceName1;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +44,14 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        if(getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
 
         HashMap<String, Integer> imageFile = new HashMap<String, Integer>();
         imageFile.put("Dimanapun Kapanpun", R.drawable.rsz_1cleaning_services);
